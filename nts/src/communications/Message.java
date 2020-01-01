1a10762b852be599aa571b3e24d4239dc2258529
Dylan Caron <dcaron81.dc@gmail.com>
2020-01-01 15:35:01 +0100

    Update Message.java
    + Constructeur(author, content)
    + getDate()
    + getContent()
    + getAuthor()

--- nts/src/communications/Message.java
+++ nts/src/communications/Message.java
@@ -1,5 +1,45 @@
 package communications;
 
-public class Message {
+import java.util.Calendar;
+import java.util.Date;
 
+import user.User;
+
+public class Message {
+    private int id;
+    private Date date;
+    private User author;
+    private String content;
+
+    public Message(User author, String content) {
+        this.author = author;
+        this.content = content;
+        this.date = Calendar.getInstance().getTime();
+        this.id = -1;
+    }
+
+    public Message(String messageInfo) {
+        // TODO Message constructeur via messageInfo
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @return the author
+     */
+    public User getAuthor() {
+        return author;
+    }
 }
