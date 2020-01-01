86216225664d4c17e2a1dc668cf547cd0be80394
Dylan Caron <dcaron81.dc@gmail.com>
2020-01-01 15:40:29 +0100

    Update User.java
    + Constructeur(login, firstName, lastName)
    + getFirstName()
    + getLastName()

--- nts/src/user/User.java
+++ nts/src/user/User.java
@@ -1,5 +1,27 @@
 package user;
 
 public class User {
+    private String login;
+    private String firstName;
+    private String lastName;
 
+    public User(String login, String firstName, String lastName) {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
 }
