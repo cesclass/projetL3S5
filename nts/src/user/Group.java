fdf6a2f84bb13732aabb2394a341536b0f6770ed
Dylan Caron <dcaron81.dc@gmail.com>
2020-01-02 17:11:48 +0100

    Create Group.java

---
+++ nts/src/user/Group.java
@@ -0,0 +1,20 @@
+package user;
+
+public class Group {
+    private String name;
+    private String description;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+}
