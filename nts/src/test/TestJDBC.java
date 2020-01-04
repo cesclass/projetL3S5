cda400a4129293de6225367b92ba2468f1e94192
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2020-01-04 01:18:48 +0100

    First prototype for JDBC

---
+++ nts/src/test/TestJDBC.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.sql.*;
+
+public class TestJDBC {
+
+	static Connection myConn = null;
+	static PreparedStatement myStmt = null;
+	static ResultSet myRs = null;
+
+	static String bdd = "jdbc:mysql://10.11.12.50:3306/nts";
+	static String id = "neocampus";
+	static String mdp = "neocampus";
+
+	public static void main(String[] args) throws SQLException {
+
+		try {
+			// Connexion
+			myConn = DriverManager.getConnection(bdd, id, mdp);
+
+			// Requette
+			myStmt = myConn.prepareStatement("SELECT `first_name`,`last_name` FROM `users`");
+			myRs = myStmt.executeQuery();
+
+			// Affichage
+			while(myRs.next()){
+				System.out.println(
+						myRs.getString("first_name") + " " + 
+						myRs.getString("last_name")
+				);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+
+	}
+
+}
