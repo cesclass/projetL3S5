1ec3cf1255bbbbe73cfbfb09e03c389902bd3453
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2020-01-04 19:09:44 +0100

    Create package "exchange"
    Ajouts
    - Package exchange
    -- Enum ComType
    -- Classe ComData
    -- Classe Serializer

---
+++ nts/src/exchange/ComData.java
@@ -0,0 +1,12 @@
+package exchange;
+
+import com.google.gson.Gson;
+
+public class ComData {
+
+    static Gson gson = new Gson();
+
+    public ComData() {
+        super();
+    }
+}
---
+++ nts/src/exchange/ComType.java
@@ -0,0 +1,5 @@
+package exchange;
+
+public enum ComType {
+
+}
