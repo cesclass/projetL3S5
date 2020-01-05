7d5599c32d74e4c98e354ca5f767a97dacde88e8
Dylan Caron <dcaron81.dc@gmail.com>
2020-01-05 22:55:21 +0100

    Update TestClientApplication.java

--- nts/src/test/TestClientApplication.java
+++ nts/src/test/TestClientApplication.java
@@ -5,15 +5,19 @@ import interfaces.UserInterface;
 import user.Group;
 
 public class TestClientApplication {
-	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		UserInterface ui = new UserInterface();
-		ui.createTicket(new Group("L3-INFO-TDA4", "groupe L3-INFO-TDA4"), 
+		Group g = new Group("L3-INFO-TDA4", "groupe L3-INFO-TDA4");
+		ui.createTicket(g, 
 				"changement de salle",
 				"Bonjour\n"
 				+ "Je vous signale que le cours de Graphe aura lieu "
 				+ "en U3-208\n"
 				+ "Merci d'en prendre compte");
+		ui.getTicketManager().getTicket(g, 0).addMessage(
+				new communications.Message(ui.getUser(),
+				"Changement de salle annulé.\n" +
+				"Le cours aura lieux demain..."));	
 
 		ClientApplication app = new ClientApplication(ui);
 
