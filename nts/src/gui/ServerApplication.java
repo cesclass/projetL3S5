f0c4c1059ff32cfbfc573b702976ea0733ef7483
TankAttaque <48088553+TankAttaque@users.noreply.github.com>
2020-01-06 18:13:09 +0100

    Update ServerApplication.java
    Ajout :
    -evenement send button
    -gestion lecture commande

--- nts/src/gui/ServerApplication.java
+++ nts/src/gui/ServerApplication.java
@@ -3,15 +3,27 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class ServerApplication {
+	//Constantes
+	public final static int MAX_SIZE_LOGIN = 8;
+	public final static int MAX_SIZE_PASSWORD = 32;
+	public final static int MAX_SIZE_FIRSTNAME = 32;
+	public final static int MAX_SIZE_LASTNAME = 32;
+	public final static int MAX_SIZE_NAME = 32;
+	public final static int MAX_SIZE_INFO = 128;
+	
     // Containers
     private JFrame mainWindow;
     private JPanel mainPanel;
     private JPanel displayPanel;
     private JPanel promptPanel;
+    private JScrollPane scrollPane;
 
     // Label
     private JLabel promptLabel;
@@ -45,7 +57,7 @@ public class ServerApplication {
 		setLayout();
 
 		// Set Event
-		//this.addEventListeners();
+		this.addEventListeners();
 
 		mainWindow.pack();
     }
@@ -83,6 +95,7 @@ public class ServerApplication {
         mainPanel = new JPanel();
         displayPanel = new JPanel();
         promptPanel = new JPanel();
+        scrollPane = new JScrollPane(displayArea);
 	}
 
 	/**
@@ -147,9 +160,9 @@ public class ServerApplication {
 				.createMatteBorder(1, 0, 0, 0, Color.BLACK));
 		promptField.setBorder(BorderFactory
 				.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-
-
 	}
+	
+
 
 	// ************************************************************************
 	// *
@@ -193,13 +206,13 @@ public class ServerApplication {
 		// Horizontal group
 		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 		hGroup.addGroup(layout.createParallelGroup()
-				.addComponent(displayArea));
+				.addComponent(scrollPane));
 		layout.setHorizontalGroup(hGroup);
 
 		// Vertical group
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
 		vGroup.addGroup(layout.createParallelGroup()
-				.addComponent(displayArea));
+				.addComponent(scrollPane));
 		layout.setVerticalGroup(vGroup);
 	}
 
@@ -231,4 +244,258 @@ public class ServerApplication {
 				.addComponent(sendButton));
 		layout.setVerticalGroup(vGroup);
 	}
+	
+	// ************************************************************************
+	// *
+	// * EVENT
+	// *
+	// ************************************************************************
+	
+	private void addEventListeners() {
+		addEventListenerSendButton();
+	}
+	
+	private void addEventListenerSendButton() {
+		this.sendButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				if(!promptField.getText().isEmpty()) {
+					commandsCase();
+				}
+			}
+			
+			/**
+			 * Check the command use
+			 */
+			private void commandsCase() {
+				String chaine = promptField.getText();
+				String tab[] = chaine.split(" ");
+				switch(tab[0]) {
+					case "users" :
+						printUsers();
+						break;
+					case "adduser" :
+						addUser(tab);
+						break;
+					case "deluser" :
+						delUser(tab);
+						break;
+					case "groups" :
+						printGroups();
+						break;
+					case "addgroup" :
+						addGroup(tab);
+						break;
+					case "delgroup" :
+						delGroup(tab);
+						break;
+					case "groupusers" :
+						printGroupUsers(tab);
+						break;
+					case "usergroups" :
+						printUserGroups(tab);
+						break;
+					case "addusergroup" :
+						addUserGroup(tab);
+						break;
+					case "delusergroup" :
+						delUserGroup(tab);
+						break;
+					default :
+						help();
+						break;
+				}
+			}
+			
+			/**
+			 * Affichage liste d'utilisateurs
+			 */
+			private void printUsers() {
+				displayArea.setText(displayArea.getText() + "printUsers : Ok!\n");
+				promptField.setText("");
+			}
+			
+			/**
+			 * Ajout utilisateur
+			 */
+			private void addUser(String[] tab) {
+				if(tab.length == 6 &&
+						tab[1].length() <= MAX_SIZE_LOGIN &&
+						tab[2].length() <= MAX_SIZE_PASSWORD &&
+						tab[3].length() <= MAX_SIZE_FIRSTNAME &&
+						tab[4].length() <= MAX_SIZE_LASTNAME &&
+						(tab[5].equals("true") || tab[5].equals("false"))) {
+					
+					displayArea.setText(displayArea.getText() + "addUser : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Suppression utilisateur
+			 */
+			private void delUser(String[] tab) {
+				if(tab.length == 2 &&
+						tab[1].length() <= MAX_SIZE_LOGIN) {
+					
+					displayArea.setText(displayArea.getText() + "delUser : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Affichage liste de groupe
+			 */
+			private void printGroups() {
+				displayArea.setText(displayArea.getText() + "printGroups : Ok!\n");
+				promptField.setText("");
+			}
+			
+			/**
+			 * Ajout d'un groupe
+			 */
+			private void addGroup(String[] tab) {
+				if(tab.length >= 4 &&
+						tab[1].length() <= MAX_SIZE_NAME &&
+						(tab[tab.length-1].equals("true") || tab[tab.length-1].equals("false"))) {
+					
+					int descriptionSize = 0;
+					for(int i = 2 ; i < tab.length-1 ; i++) {
+						descriptionSize += tab[i].length();
+					}
+					if(descriptionSize <= MAX_SIZE_INFO) {
+						displayArea.setText(displayArea.getText() + "addGroup : Ok!\n");
+						promptField.setText("");
+					}else {
+						help();
+					}
+					
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Suppression d'un groupe
+			 */
+			private void delGroup(String[] tab) {
+				if(tab.length == 2 && 
+						tab[1].length() <= MAX_SIZE_NAME) {
+					
+					displayArea.setText(displayArea.getText() + "delGroup : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Affichage des utilisateurs d'un groupe
+			 */
+			private void printGroupUsers(String[] tab) {
+				if(tab.length == 2 && 
+						tab[1].length() <= MAX_SIZE_NAME) {
+					
+					displayArea.setText(displayArea.getText() + "printGroupUsers : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Affichage des groupes d'un utilisateur
+			 */
+			private void printUserGroups(String[] tab) {
+				if(tab.length == 2 && 
+						tab[1].length() <= MAX_SIZE_LOGIN) {
+					
+					displayArea.setText(displayArea.getText() + "printUserGroups : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Ajout d'un utilisateur dans un groupe
+			 */
+			private void addUserGroup(String[] tab) {
+				if(tab.length == 3 && 
+						tab[1].length() <= MAX_SIZE_LOGIN &&
+						tab[2].length() <= MAX_SIZE_NAME) {
+					
+					displayArea.setText(displayArea.getText() + "addUserGroup : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Suppression d'un utilisateur dans un groupe
+			 */
+			private void delUserGroup(String[] tab) {
+				if(tab.length == 3 && 
+						tab[1].length() <= MAX_SIZE_LOGIN &&
+						tab[2].length() <= MAX_SIZE_NAME) {
+					
+					displayArea.setText(displayArea.getText() + "delUserGroup : Ok!\n");
+					promptField.setText("");
+				}else {
+					help();
+				}
+			}
+			
+			/**
+			 * Affiche les commandes
+			 */
+			private void help() {
+				displayArea.setText(displayArea.getText() +
+						"// list commands and usages\n" + 
+						"help\n" + 
+						"\n" + 
+						"// list users\n" + 
+						"users\n" + 
+						"// add user\n" + 
+						"adduser \"login\" \"password\" \"firstname\" \"lastname\" \"istech\"\n" + 
+						"// delete user\n" + 
+						"deluser \"login\"\n" + 
+						"\n" + 
+						"// list groups\n" + 
+						"groups\n" + 
+						"// add group\n" + 
+						"addgroup \"name\" \"description\" \"is_tech\"\n" + 
+						"// delete group\n" + 
+						"delgruop \"name\"\n" + 
+						"\n" + 
+						"// list users in group\n" + 
+						"groupusers \"name\"\n" + 
+						"// list groups of user\n" + 
+						"usergroups \"login\"\n" + 
+						"// add user in group\n" + 
+						"addusergroup \"login\" \"name\"\n" + 
+						"// del user from group\n" + 
+						"delusergroup \"login\" \"name\"\n" + 
+						"\n" + 
+						"Specs :\n" + 
+						"  sizes :\n" + 
+						"  - login <= 8\n" + 
+						"  - password <= 32\n" + 
+						"  - firstname <= 32\n" + 
+						"  - lastname <= 32\n" + 
+						"  - name <= 32\n" + 
+						"  - info <= 128\n" + 
+						"  restrictions :\n" + 
+						"  - istech = (true | false)" +
+						"\n");
+				promptField.setText("");
+			}
+		});
+	}
+	
 }
