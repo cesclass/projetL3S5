3db39dc60e1c6c8ee18cdb4160d4df12f8a0eb03
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2020-01-07 18:14:24 +0100

    Update Serializer
    MaJ
    - Serializer a maintenant des methodes statiques

--- nts/src/exchange/Serializer.java
+++ nts/src/exchange/Serializer.java
@@ -5,11 +5,11 @@ import com.google.gson.*;
 public class Serializer {
     private static Gson gson = new Gson();
 
-    public String serialize(ComData data) {
+    public static String serialize(ComData data) {
         return gson.toJson(data);
     }
 
-    public ComData deserialize(String jsonString) {
+    public static ComData deserialize(String jsonString) {
         return gson.fromJson(jsonString, ComData.class);
     }
     
