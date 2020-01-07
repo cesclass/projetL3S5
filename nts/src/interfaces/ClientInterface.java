bf438e44446567b376e6afde2fe4b1081ec97902
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2020-01-07 18:18:21 +0100

    Update ClientInterface
    MaJ
    - ClientInterface (Socket, Thread)

--- nts/src/interfaces/ClientInterface.java
+++ nts/src/interfaces/ClientInterface.java
@@ -1,5 +1,38 @@
 package interfaces;
 
-public class ClientInterface {
+import java.io.*;
+import java.net.*;
+// import java.util.*;
 
+public class ClientInterface implements Runnable {
+
+    // static Map<Integer, Socket> client_table = new HashMap<>();
+
+    private Socket client = null;
+    // private PrintWriter writer = null;
+    // private BufferedInputStream reader = null;
+
+    public ClientInterface(Socket client) {
+        // client_table.put(client.getPort(), client);
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+            System.out.println("client port: " + client.getPort());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        // } finally {
+        //     client_table.remove(client.getPort());
+        }
+    }
+    
 }
